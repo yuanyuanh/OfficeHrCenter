8067c46133449637fca121739ff3778fe014c2af
wenqi <wang_wenq@bentley.edu>
2020-04-27 15:01:39 -0400

    correct the directory

--- app/src/main/java/com/example/officehrcenter/activities/BookingActivity.java
+++ app/src/main/java/com/example/officehrcenter/activities/BookingActivity.java
@@ -2,6 +2,7 @@ package com.example.officehrcenter.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
